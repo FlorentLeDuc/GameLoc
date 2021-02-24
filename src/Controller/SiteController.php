18a4b7b2ea09d237fb6a8291edf1a82add8e13c2
Florent <florentducourneau@live.fr>
2021-02-24 17:37:30 +0100

    routes

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -8,10 +8,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SiteController extends AbstractController
 {
-    #[Route('/site', name: 'site')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('site/index.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    #[Route('/user', name: 'user')]
+    public function user(): Response
+    {
+        return $this->render('site/user.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+    #[Route('/game', name: 'game')]
+    public function game(): Response
+    {
+        return $this->render('site/game.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+    #[Route('/category', name: 'category')]
+    public function category(): Response
+    {
+        return $this->render('site/category.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+    #[Route('/offer', name: 'offer')]
+    public function offer(): Response
+    {
+        return $this->render('site/offer.html.twig', [
             'controller_name' => 'SiteController',
         ]);
     }
