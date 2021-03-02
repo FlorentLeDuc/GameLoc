cb30f411e2dc26af1142cf2bf5b951f3d1df1db6
Florent <florentducourneau@live.fr>
2021-03-02 09:54:49 +0100

    game

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/game')]
+#[Route('/admin/game')]
 class GameController extends AbstractController
 {
     #[Route('/', name: 'game_index', methods: ['GET'])]
