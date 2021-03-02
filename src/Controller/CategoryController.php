a4ea4e42b0c898e9cddf9f0adac86fdc6837bb83
Florent <florentducourneau@live.fr>
2021-03-02 16:17:49 +0100

    category dans game

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -18,6 +18,7 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
+            
         ]);
     }
 
