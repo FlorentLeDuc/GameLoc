4ac5e3f65ed93ceeee95d39e16b988cbdada7477
Florent <florentducourneau@live.fr>
2021-03-03 16:16:31 +0100

    ajout mail prise de contact par un offer

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Offer;
 use App\Form\OfferType;
+use App\Form\OfferContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\OfferRepository;
 use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 class SiteController extends AbstractController
 {
@@ -66,6 +70,7 @@ class SiteController extends AbstractController
     #[Route('/offers', name: 'offers')]
     public function offers(OfferRepository $offerRepository): Response
     {
+
         $offers = $offerRepository->findBy([], [ "publication_date" => "DESC"]);
         return $this->render('site/offers.html.twig', [
             'controller_name' => 'SiteController',
@@ -73,11 +78,32 @@ class SiteController extends AbstractController
         ]);
     }
 
-    #[Route('/offer{id}', name: 'offer', methods: ['GET'])]
-    public function offer(Offer $offer): Response
+    #[Route('/offer{id}', name: 'offer', methods: ['GET', 'POST'])]
+    public function offer(Offer $offer, Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(OfferContactType::class);
+
+        $contact = $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $email = (new TemplatedEmail())
+                ->from($contact->get('email')->getData())
+                ->to($offer->getUser()->getEmail())
+                ->subject('Contact au sujet de votre annonce "' . $offer->getTitle() . '"')
+                ->htmlTemplate('emails/contact_annonce.html.twig')
+                ->context([
+                    'offer' => $offer,
+                    'mail' => $contact->get('email')->getData(),
+                    'message' => $contact->get('message')->getData()
+                ]);
+            $mailer->send($email);
+
+            $this->addFlash('message', 'Votre e-mail a bien été envoyé');
+        }
+
         return $this->render('site/offer.html.twig', [
             'offer' => $offer,
+            'form' => $form->createView()
         ]);
     }
 
